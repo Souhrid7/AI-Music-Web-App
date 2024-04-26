141acb03f358d2406e2f22ef92c9ef37254b5947
Souhrid7 <153090885+Souhrid7@users.noreply.github.com>
2024-04-26 18:23:08 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,39 @@
+song=""
+leftWristX="" 
+leftWristY=""
+rightWristX=""
+rightWristY=""
+function preload() {
+    song=loadSound("music.mp3")
+}
 function setup() {
-    canvas=createCanvas(500,250)
+    canvas=createCanvas(400,400);
+    canvas.center()
+    video=createCapture(VIDEO)
+    video.hide()
+    ps=ml5.poseNet(video,modelLoaded)
+    ps.on('pose',gotPoses)
+}
+function draw() {
+    image(video,0,0,400,400)
+}
+function play() {
+    song.play()
+    song.setVolume(0.2)
+    song.rate(1.3)
+}
+function modelLoaded() {
+    console.log("model is loaded")
+}
+function gotPoses(results) {
+    if(results.length>0) {
+        //console.log(results)
+        leftWristX=results[0].pose.leftWrist.x
+        leftWristY=results[0].pose.leftWrist.y 
+        console.log(leftWristX)
+        console.log(leftWristY)
+        rightWristX=results[0].pose.rightWrist.x
+        rightWristY=results[0].pose.rightWrist.y
+        console.log("rightWrist " + rightWristX+","+rightWristY)
+    }
 }
