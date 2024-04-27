a4174dc07450cd9609bc984c233eb1e253e44381
Souhrid7 <153090885+Souhrid7@users.noreply.github.com>
2024-04-27 17:48:34 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,21 +1,40 @@
-song=""
-leftWristX="" 
-leftWristY=""
-rightWristX=""
-rightWristY=""
+song = ""
+leftWristX = ""
+leftWristY = ""
+rightWristX = ""
+rightWristY = ""
+scoreleftwrist = ""
+isplaying = ""
+
 function preload() {
-    song=loadSound("music.mp3")
+    song = loadSound("music.mp3")
+    song2 = loadSound("music2.mp3")
 }
 function setup() {
-    canvas=createCanvas(400,400);
+    canvas = createCanvas(600, 500);
     canvas.center()
-    video=createCapture(VIDEO)
+    video = createCapture(VIDEO)
     video.hide()
-    ps=ml5.poseNet(video,modelLoaded)
-    ps.on('pose',gotPoses)
+    ps = ml5.poseNet(video, modelLoaded)
+    ps.on('pose', gotPoses)
 }
 function draw() {
-    image(video,0,0,400,400)
+    image(video, 0, 0, 600, 500)
+    fill("red")
+    stroke("red")
+    if (scoreleftwrist > 0.2) {
+        circle(leftWristX, leftWristY, 25)
+        n = Number(leftWristY)
+        r = floor(n)
+        if(song.isPlaying()) {
+            song.pause()
+            song2.play()
+        }
+        if(song2.isPlaying()) {
+            song2.pause()
+            song.play()
+        }
+    }
 }
 function play() {
     song.play()
@@ -26,14 +45,15 @@ function modelLoaded() {
     console.log("model is loaded")
 }
 function gotPoses(results) {
-    if(results.length>0) {
+    if (results.length > 0) {
         //console.log(results)
-        leftWristX=results[0].pose.leftWrist.x
-        leftWristY=results[0].pose.leftWrist.y 
+        scoreleftwrist = results[0].pose.keypoints[9].score
+        leftWristX = results[0].pose.leftWrist.x
+        leftWristY = results[0].pose.leftWrist.y
         console.log(leftWristX)
         console.log(leftWristY)
-        rightWristX=results[0].pose.rightWrist.x
-        rightWristY=results[0].pose.rightWrist.y
-        console.log("rightWrist " + rightWristX+","+rightWristY)
+        rightWristX = results[0].pose.rightWrist.x
+        rightWristY = results[0].pose.rightWrist.y
+        console.log("rightWrist " + rightWristX + "," + rightWristY)
     }
 }
